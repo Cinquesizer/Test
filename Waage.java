02c944be561eb9a14dc6bbeb13ee9950f0d7c173
Josia Kämper <JosiKaempe19@ahfbkdt.de>
2019-12-13 09:40:39 +0100

    Waage Übungen

---
+++ Waage.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Waage here.
+ * 
+ * @author Josia Kaemper 
+ * @version V.1.0 13.12.2019
+ */
+public class Waage
+{
+    /**
+     * Constructor for objects of class Waage
+     */
+    public Waage()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
